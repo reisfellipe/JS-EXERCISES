4f3259df145b73828749fe1e8289734fbcab2abb
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-06 20:30:53 -0300

    doing the sum function

---
+++ ex08/scripts.js
@@ -0,0 +1,12 @@
+// var nome = prompt("Qual é o seu nome?");
+// alert(`Seja bem vindo, ${nome}`);
+
+//pedir para o usuário digitar um numero
+//pedir para o usuário digitar outro numero
+//mostrar a soma
+
+let n1 = Number(prompt('Digite um número:'));
+let n2 = Number(prompt('Digite outro número:'));
+const soma = (x,y) => x + y;
+
+alert(`A soma de ${n1} com ${n2} é igual a: ${soma(n1,n2)}`);

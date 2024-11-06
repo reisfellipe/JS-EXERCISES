8b5ab42632556fe1e03d09909db345d7060c0ef1
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-06 20:45:59 -0300

    convertendo tipos

--- ex08/scripts.js
+++ ex08/scripts.js
@@ -9,4 +9,7 @@ let n1 = Number(prompt('Digite um número:'));
 let n2 = Number(prompt('Digite outro número:'));
 const soma = (x,y) => x + y;
 
+// Jeito mais simples que mostrar a soma seria
+// alert( n1 + n2 )
+
 alert(`A soma de ${n1} com ${n2} é igual a: ${soma(n1,n2)}`);

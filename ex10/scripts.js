661088bd439d398a9bca4f2bfb2f8af2b286afd9
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-06 21:54:22 -0300

    novo exercicio

---
+++ ex10/scripts.js
@@ -0,0 +1,16 @@
+/*
+    EXERCÍCIO PRÁTICO;
+    -Pedir para o usuario digitar um numero
+    -Converter em numero inteiro
+    -Pedir ao usuário que digite outro numero
+    -converter em numero inteiro
+
+    -> Mostrar a subtração concatenando com o texto "a subtração é:..."
+    
+    -> Mostrar a multiplicação concatenando com o texto "a multiplicação é:..."
+
+    -Mostrar a divisão concatenando com o texto "a divisão é:.."
+
+    -Mostrar a soma concatenando com o texto "a soma é:..."
+*/
+

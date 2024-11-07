ab9d1d4bdf2b14159eba20d76c459711ebe7553e
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-06 21:32:42 -0300

    done

--- ex09/scripts.js
+++ ex09/scripts.js
@@ -13,3 +13,14 @@
 
     -Mostrar a soma concatenando com o texto "a soma é:..."
 */
+
+let n1 = Number(prompt('Digite um numero:'));
+let n2 = Number(prompt('Digite outro numero:'));
+
+const subtracao = (x,y) => x - y;
+const soma = (x,y) => x + y;
+const multiplicacao = (x,y) => x * y;
+
+alert(`A subtração de ${n1} por ${n2} é: ${subtracao(n1,n2)}`);
+alert(`A soma de ${n1} com ${n2} é igual a: ${soma(n1,n2)}`);
+alert(`A multiplicação de ${n1} vezes ${n2} é igual a: ${multiplicacao(n1,n2)}`);

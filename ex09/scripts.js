1e525e560566f98bc51b6d7ea94ffe9159a60ed1
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-06 21:46:19 -0300

    divisao

--- ex09/scripts.js
+++ ex09/scripts.js
@@ -20,7 +20,9 @@ let n2 = Number(prompt('Digite outro numero:'));
 const subtracao = (x,y) => x - y;
 const soma = (x,y) => x + y;
 const multiplicacao = (x,y) => x * y;
+const divisao = (x,y) => x / y;
 
 alert(`A subtração de ${n1} por ${n2} é: ${subtracao(n1,n2)}`);
 alert(`A soma de ${n1} com ${n2} é igual a: ${soma(n1,n2)}`);
 alert(`A multiplicação de ${n1} vezes ${n2} é igual a: ${multiplicacao(n1,n2)}`);
+alert(`A divisão entre ${n1} e ${n2} é: ${divisao(n1,n2)}`);

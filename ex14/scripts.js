10d07cde6489c7b7e0e29d100704b7f61592f9ab
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-09 20:10:05 -0300

    creating new exercise

---
+++ ex14/scripts.js
@@ -0,0 +1,12 @@
+/*
+    EXERCÍCIO PRÁTICO;
+    - Fazer jogador1 digitar um numero
+    - fazer jogador2 digitar outro numero
+    - SE forem iguais, mostrar mensagem "empate"
+    - Sortear um numero entre 0 e 1
+
+    - SE o numero sorteado for 0, ganha quem escolher o numero MENOR
+    -SE o numero sorteado for 1, ganha quem escolher o numero MAIOR
+
+*/
+

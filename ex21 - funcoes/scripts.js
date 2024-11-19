5c938abd26a854cb357fd67c58dd5f236305f029
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-19 20:06:40 -0300

    prreview of challenge

--- ex21 - funcoes/scripts.js
+++ ex21 - funcoes/scripts.js
@@ -1,4 +1,19 @@
 /*
     EXERCÍCIO PRÁTICO;
-    M.
+    Pedir para o usuário digitar um número e mostrar a tabuada desse numero de 1 a 1000.
+
+    deve ter a possibilidade do usuário digitar outro número e também de limpar a tabuada gerada previamente.
+
 */
+
+const output = document.getElementById('output'); //Não será necessário usar o # para setar o elemento, pois estamos usando o getElementById
+
+let msg = '';
+
+console.log(output);
+
+
+for(let i = 0; i <= 1001; i++){
+    msg += "i: " + i+ '<br>';
+}
+output.innerHTML = msg;

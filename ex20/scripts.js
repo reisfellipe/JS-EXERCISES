31bfb0ae07d223c71a779888c5712fc084851e7a
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-19 19:46:55 -0300

    é bissexto? feito!

--- ex20/scripts.js
+++ ex20/scripts.js
@@ -3,9 +3,14 @@
     M.
 */
 
-let ano = Number(prompt("Digite o ano de onde deseja começar:"));
+let ano = 1004;
 
 while( ano <= 2024 ){
-    document.write(`${ano} <br>`);
-    ano += 100;
+
+    if( !(ano % 100 === 0) || (ano % 400 === 0) ){
+        document.write(`${ano} <br>`);
+    }else{
+        document.write(`${ano} não é bissexto <br>`)
+    }
+    ano += 4;
 }

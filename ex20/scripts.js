172db15078c15e3e7ad7dcffa2c91b5019bf67ab
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-19 19:38:14 -0300

    increasing exercises

---
+++ ex20/scripts.js
@@ -0,0 +1,11 @@
+/*
+    EXERCÍCIO PRÁTICO;
+    M.
+*/
+
+let ano = Number(prompt("Digite o ano de onde deseja começar:"));
+
+while( ano <= 2024 ){
+    document.write(`${ano} <br>`);
+    ano += 100;
+}

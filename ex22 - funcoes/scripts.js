c8801072a3582cdce24c7a9c113d961f4d4e37e0
Fellipe Reis <reisfellipe11@gmail.com>
2024-11-19 21:49:16 -0300

    updating

---
+++ ex22 - funcoes/scripts.js
@@ -0,0 +1,30 @@
+/*
+    EXERCÍCIO PRÁTICO;
+    Pedir para o usuário digitar um número e mostrar a tabuada desse numero de 1 a 1000.
+
+    deve ter a possibilidade do usuário digitar outro número e também de limpar a tabuada gerada previamente.
+
+*/
+
+
+function mostrarTabuada(){
+    
+    let numero = parseInt(prompt("Digite um número para a tabuada:"));
+    let max = parseInt(prompt("Digite o máximo para a tabuada:")) || 1000;
+
+    for(let i = 1; i <= max; i++){
+        mensagem =+ `${numero} x ${i} = ${numero * i}`;
+        
+    }
+
+    output.innerHTML = mensagem;
+}
+
+
+function limparTabuada(){
+    mensagem = '';
+    output.innerHTML = mensagem;
+}
+
+const output = document.getElementById('output');
+var mensagem = '';

20c2cb9c894e2984a8cef3473091b13cbafb84f4
Fellipe Reis <reisfellipe11@gmail.com>
2025-02-04 21:25:11 -0300

    saving progress

--- ex24/script.js
+++ ex24/script.js
@@ -2,32 +2,41 @@
 //callback > promise > async/await
 
 const numero = document.getElementById('numero');
+const btnPromessa = document.getElementById('btn-promessa');
 
-//Criando a promessa
-let promise = new Promise((resolve, reject) => {
-    let resultado = true;
-    let tempo = 3000;
+btnPromessa.addEventListener("click",()=>{
+    //Criando a promessa
+    numero.innerHTML = "Processando..."
+    promessa().then((retorno) => {
+        numero.innerHTML = retorno;
+        numero.classList.remove("erro"),
+        numero.classList.add("ok");
+        console.log(retorno);
+    }).catch((e) => {
+        numero.innerHTML = e;
+        numero.classList.add("erro");
+        numero.classList.remove("ok");
     
-    setTimeout(() => {
-        if(resultado){
-            resolve("Deu tudo certo.")
-        }else{
-            reject("Deu tudo errado.")
-        }
-    }, tempo);
-}); 
-
-promise.then((retorno) => {
-    numero.innerHTML = retorno;
-    numero.classList.remove("erro"),
-    numero.classList.add("ok");
-    console.log(retorno);
-}).catch((e) => {
-    numero.innerHTML = e;
-    numero.classList.add("erro");
-    numero.classList.remove("ok");
-
+    })
 })
 
 
-numero.innerHTML = "Processando..."
+const promessa = () => {
+    
+    let p = new Promise((resolve, reject) => {
+        let resultado = true;
+        let tempo = 3000;
+        
+        setTimeout(() => {
+            if(resultado){
+                resolve("Deu tudo certo.")
+            }else{
+                reject("Deu tudo errado.")
+            }
+        }, tempo);
+    }); 
+
+    return p;
+}
+
+numero.innerHTML = "Esperando"
